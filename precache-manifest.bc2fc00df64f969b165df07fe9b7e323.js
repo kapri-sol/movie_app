45f2090bbce0fc377cc0182afd3440874c7792eb
van <ksm6902@gmail.com>
2019-08-09 20:50:02 +0900

    Updates

---
+++ precache-manifest.bc2fc00df64f969b165df07fe9b7e323.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638cdfdd7bb2c0229113a10e132d1176",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "5f1643b9ed2c4145d8ef",
+    "url": "/movie_app/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "5dd58ee1315614173bdc",
+    "url": "/movie_app/static/js/2.6afeaf01.chunk.js"
+  },
+  {
+    "revision": "5f1643b9ed2c4145d8ef",
+    "url": "/movie_app/static/js/main.f85b65d5.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
